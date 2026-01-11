7dcf965395dc851b2f3607a3979146e9cadfd51e
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-11 22:13:39 +0000

    Lesson about user input strings

---
+++ Lessons/strings/input.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string fullName;
+
+    cout << "Type your full name: ";
+    getline(cin, fullName);
+    
+    cout << "Your name is: " + fullName + "\n";
+}
