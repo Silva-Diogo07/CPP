8de49c0bdd68d54248c05489248db94dce9e4587
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-11 21:27:35 +0000

    Lesson about string size

---
+++ Lessons/strings/length.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    cout << "The length of the txt string is " << txt.length() << "\n";
+    cout << "The length of the txt string is " << txt.size() << "\n";
+
+    return 0;
+}
