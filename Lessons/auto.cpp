92a6804a450a62898ca222e9cf664484b7945c24
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-11 18:48:14 +0000

    Lesson about 'auto' keyword

---
+++ Lessons/auto.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    auto myNum = 5; // int
+    auto myFloatNum = 5.99f; // float
+    auto myDoubleNum = 9.98; // double
+    auto myLetter = 'D'; // char
+    auto myBoolean = true; // bool
+    auto myString = "Hello"; // string
+}
+
+/* NOTE: auto only works when assigned a value and once the type is choosen it stays the same */
