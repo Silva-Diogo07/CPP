738ef01ac4ba2b21572be762b72185961de62f25
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-11 18:42:35 +0000

    Lessons

---
+++ Lessons/display.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+int main()
+{    
+    std::string name = "Diogo";
+    int age = 18;
+
+    std::cout << "Hello World!\n";
+    std::cout << age << "\n";
+
+    std::cout << "Hello " << name << " you are " << age << " years old\n";
+
+    return 0;
+}
