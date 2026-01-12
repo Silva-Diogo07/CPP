cd575c9794542b13929b690146b783c341a1cd35
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-12 14:09:44 +0000

    Lesson about math

---
+++ Lessons/math.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    cout << max(5,10) << "\n";
+    cout << min(41,13) << "\n";
+
+    int x = 10, y = 15;
+
+    cout << max(x, y) << "\n";
+    cout << min(x, y) << "\n";
+
+    cout << sqrt(64) << "\n";
+}
