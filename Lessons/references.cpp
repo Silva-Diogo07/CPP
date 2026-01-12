d707ad3a1fef5459cea1e27d7d4be59dc85b77a9
Silva-Diogo07 <2007.silva.diogo@gmail.com>
2026-01-12 20:46:29 +0000

    References

---
+++ Lessons/references.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string food = "Pizza";
+    string &meal = food;
+
+    meal = "Burger";
+
+    cout << food << "\n";
+    cout << meal << "\n";
+
+    return 0;
+}
